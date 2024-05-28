2ca99f88fceae9658234aa4b7005adfaa142b5bb
Mark Nguyen <markvn08@gmail.com>
2024-05-28 00:34:28 -0700

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO) // connects to database. NOTE: by using env variable, we don't upload the key to github.
+  .then(() => {
+    console.log("Connected to MongoDB"); // prints success message to console
+  })
+  .catch((err) => {
+    console.log(err); // prints error message to console
+  });
 
 const app = express();
 
