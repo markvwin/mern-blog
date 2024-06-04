4ca7a5546305e046012b67d4d301b500f4eafe59
Mark Nguyen <markvn08@gmail.com>
2024-06-03 22:48:10 -0700

    implemented redux-persist

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// if you have more than one reducer, you need to combine them into one reducer
+
+const rootReducer = combineReducers({
+  user: userReducer, // renaming userReducer to user
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer, // renaming userReducer to user
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }), // prevents redux-persist from throwing an error
 });
+
+export const persistor = persistStore(store);
+
+const currentState = store.getState();
+
+// Inspect the state object to check for the presence of the _persist key
+if ("_persist" in currentState) {
+  console.log("Redux Persist is working correctly!");
+} else {
+  console.log("Redux Persist might not be configured correctly.");
+}
+
+// You can also log the entire state object for further inspection
+console.log("Current Redux Store State:", currentState);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./app/store";
+import { store, persistor } from "./app/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+/* 
+  By wrapping the application by the React-Redux <Provider>, we make
+  the redux store available to our react components.
+*/
+
+/* 
+  By wrapping the application with the Redux-Persist <PersistGate> component, 
+  we enable Redux Persist to persist and update the Redux store's current state to 
+  and from local storage respectively. 
+*/
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  /* By wrapping the application by the React-Redux <Provider>, we make
-     the redux store available to our react components.*/
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
