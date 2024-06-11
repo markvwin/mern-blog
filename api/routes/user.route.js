db7d0db0ff19e37c5a05042541a91e54fba3e8c4
Mark Nguyen <markvn08@gmail.com>
2024-06-11 12:49:17 -0700

    implemented functionality for deleting User (added deleteUser Api route)

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test); // test api route.
 router.put("/update/:userId", verifyToken, updateUser); // updates user api route.
+router.delete("/delete/:userId", verifyToken, deleteUser); // deletes user api route
 
 export default router; // exports the router so that it can be used in other files.
